60fd8d2e51ef08a935900294bc7c41b37cdc05bf
bgharashvilimarii <mariambgharashvili@gmail.com>
2025-11-09 19:13:41 +0400

    second commit

---
+++ day012/homework/main.py
@@ -0,0 +1,19 @@
+#sequencing- aris kodebis wakitxvis tanmimdevrobis algoritmi 
+print("mari")
+print("dachi")
+#iteration-ais erti da igive mokmedebis mravaljer ganmeoreba (example-gym sets)
+#selection-aris gadawkvetilebis-archevnis gaketeba migeba pirobis-kitxvis mixedvit (authorization)
+
+#alghoritm aris nabij-nabij gawerili instruqcia davalebis shesasruleblad (example:roboti)
+#alghoritm avgwert flowchart-it pseudocode-it natural language-it
+
+name = input("write your name: ")
+name1 = "mari"
+print( name1 == name)
+
+# name1 = mari
+# enter name
+# if name is equal to name1
+# print True
+# if name isnot equeal to name1
+# print false 
