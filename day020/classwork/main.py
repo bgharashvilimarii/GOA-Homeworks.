00e7eb19471c7bfdc2bf9bf49874333c188343c5
bgharashvilimarii <mariambgharashvili@gmail.com>
2025-12-13 16:08:38 +0400

    second commit

---
+++ day020/classwork/main.py
@@ -0,0 +1,52 @@
+# num1 = int(input("write first number: "))
+# num2 = int(input("write second number: "))
+# num3 = int(input("write third number: "))
+# num4 = int(input("write fourth number: "))
+# num5 = int(input("write fifth number: "))
+# sum = num1 + num2 + num3 + num4 + num5
+
+# if sum % 2 == 0:
+#     print("number is even")
+# else:
+#     print("number is odd")
+
+
+# number = int(input("write any number: "))
+
+# while number % 5 != 0 or number % 7 != 0: 
+#     number=int(input("try again: "))
+
+# print(number)
+
+
+
+
+
+# 1) ლეპტოპი - 1500ლ
+# 2) ტელეფონი - 1000ლ
+# 3) ფეხსაცმელი - 100ლ
+# 4) პერანგი - 50ლ
+# 5) რვეული - 5ლ
+
+# balance = int(input("write any number: "))
+
+# if 1500 <= balance:
+#     print("laptop")
+# elif 1000 <= balance:
+#     print("phone")
+# elif 100 <= balance:
+#     print("shoes")
+# elif 50 <= balance:
+#     print("shirt")
+# elif 5 <= balance:
+#     print("notebook")
+
+
+num = int(input("write any number: "))
+
+if num > 0:
+    print(" its a positive number")
+elif num < 0:
+    print("its a negative number")
+else:
+    print("its a zero")
---
+++ day020/homework/main.py
@@ -0,0 +1,23 @@
+# # / aris gayofa romelic erti ricxvis meoreze gakofisas zust ganayofs gvaZlevs pasuxad
+# print(10/2)
+# print(9/2)
+# print(27/7)
+# # // nishnavs ramdenjer motavsdeba erti ricxvi meoreshi
+# print(7//3)
+# print(100//5)
+# print(99//9)
+# # % gayofa gvichvenebs ramdenia erti ricxvis meoreze gayofiss migebuli nashti
+# print(9 % 3)
+# print(27 % 6)
+# print(99 % 8)
+
+# elif aris sxvashemtxvevashi tu misi daxmarebit shegvidzlia(if-else_shi) bevri pirobis shemotana
+
+year = int(input("Enter any year: "))
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print("This is a leap year")
+else:
+    print(year, "is not leap year")
+
+    
